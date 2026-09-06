f795155d408dc6fb80c5e153a18385c381b1c75d
Auto4231 <97334013+Auto4231@users.noreply.github.com>
2026-09-06 23:10:54 +0800

    Refactor JSON handling for version compatibility

--- spine-godot/spine_godot/SpineAtlasResource.cpp
+++ spine-godot/spine_godot/SpineAtlasResource.cpp
@@ -319,14 +319,22 @@ Error SpineAtlasResource::load_from_file(const String &path) {
 #endif
 
 #if VERSION_MAJOR > 3
-	JSON *json = memnew(JSON);
-	error = json->parse(json_string);
-	if (error != OK) {
-		memdelete(json);
-		return error;
-	}
-	Variant result = json->get_data();
-	memdelete(json);
+#if VERSION_MAJOR >= 4 && VERSION_MINOR >= 7
+    Ref<JSON> json;
+    json.instantiate();
+    error = json->parse(json_string);
+    if (error != OK) return error;
+    Variant result = json->get_data();
+#else
+    JSON *json = memnew(JSON);
+    error = json->parse(json_string);
+    if (error != OK) {
+        memdelete(json);
+        return error;
+    }
+    Variant result = json->get_data();
+    memdelete(json);
+#endif
 #else
 	String error_string;
 	int error_line;
@@ -376,10 +384,15 @@ Error SpineAtlasResource::save_to_file(const String &path) {
 	content["normal_texture_prefix"] = normal_map_prefix;
 	content["specular_texture_prefix"] = specular_map_prefix;
 #if VERSION_MAJOR > 3
-	JSON *json = memnew(JSON);
-	file->store_string(json->stringify(content));
-	file->flush();
-	memdelete(json);
+#if VERSION_MAJOR >= 4 && VERSION_MINOR >= 7
+    Ref<JSON> json;
+    json.instantiate();
+    file->store_string(json->stringify(content));
+#else
+    JSON *json = memnew(JSON);
+    file->store_string(json->stringify(content));
+    memdelete(json);
+#endif
 #else
 	file->store_string(JSON::print(content));
 	file->close();
